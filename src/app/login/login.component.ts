994952b4d5c2d46beeeeb803b29ab0443fb5c32c
fbettir <betti.farkas2@gmail.com>
2025-05-16 17:34:59 +0200

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { LocalStorageService } from '../services/local-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -9,4 +10,15 @@ import { environment } from '../../environments/environment';
 export class LoginComponent {
   environment = environment;
 
+  isLoggedIn: boolean = false;
+
+  constructor(private storageService: LocalStorageService){
+
+  }
+
+  onInit(){
+    if(this.storageService.getUser() != null ){
+      this.isLoggedIn = true;
+    }
+  }
 }
