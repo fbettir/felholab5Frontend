7069d094011bc346ff9b19f38ebf6ce2191e1884
fbettir <betti.farkas2@gmail.com>
2025-05-16 17:02:23 +0200

    signin

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,8 +4,18 @@ import { environment } from '../../environments/environment';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  environment = environment
+  environment = environment;
+
+  googleUser: any;
+
+  onSignIn(googleUser: any) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
 }
